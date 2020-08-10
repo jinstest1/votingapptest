a6f72ab60f9c8bc7e9ba5942a05e62fcb149fd42
jinstest1 <62053453+jinstest1@users.noreply.github.com>
2020-08-10 11:32:29 +0530

    Update Program.cs

--- worker/src/Worker/Program.cs
+++ worker/src/Worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=db;Username=postgres_user;Password=postgres_password;");
+                var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
